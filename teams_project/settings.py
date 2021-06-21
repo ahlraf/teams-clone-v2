265888dc35b3115c6316d7ea8ec3ca48d68b34f9
ahlraf <72507078+ahlraf@users.noreply.github.com>
2021-06-21 13:22:39 +0530

    collectstatic

--- teams_project/settings.py
+++ teams_project/settings.py
@@ -123,7 +123,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'teams_project/static')
+    os.path.join(BASE_DIR, 'teams_project/static'),
+    os.path.join(BASE_DIR, 'teams/static'),
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
