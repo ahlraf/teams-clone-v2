634b326b7831881ea19d7e88aaea0dccfae55a50
ahlraf <72507078+ahlraf@users.noreply.github.com>
2021-06-22 15:12:23 +0530

    setup

--- teams_project/settings.py
+++ teams_project/settings.py
@@ -146,6 +146,9 @@ CACHE_MIDDLEWARE_SECONDS = 604800
 CACHE_MIDDLEWARE_KEY_PREFIX = ''
 
 
+import django_heroku
+django_heroku.settings(locals())
+
 # import dj_database_url
 # db_from_env = dj_database_url.config(conn_max_age=500)
 # DATABASES['default'].update(db_from_env)
